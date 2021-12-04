da0c9b5d2c70ec2dfbd7d6b97a307e94500b6500
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2021-12-04 15:53:40 -0500

    finished progress changes

--- Kiosk/app/controllers/kioskpages_controller.rb
+++ Kiosk/app/controllers/kioskpages_controller.rb
@@ -2,15 +2,15 @@ class KioskpagesController < ApplicationController
     def mainapp
         @paramIn = params[:id]
         if(@paramIn == "userdetails")
-            render :template => "kioskpages/mainapp", :locals => {:temp1 => "yes", :temp2 => "no", :temp3 => "no", :temp4 => "no", :temp5 => "no"}
-        elsif(@paramIn == "userconsent")
-            render :template => "kioskpages/mainapp", :locals => {:temp1 => "no", :temp2 => "yes", :temp3 => "no", :temp4 => "no", :temp5 => "no"}
-        elsif(@paramIn == "userinsurance") 
-            render :template => "kioskpages/mainapp", :locals => {:temp1 => "no", :temp2 => "no", :temp3 => "yes", :temp4 => "no", :temp5 => "no"}
-        elsif(@paramIn == "userhealth")
             render :template => "kioskpages/mainapp", :locals => {:temp1 => "no", :temp2 => "no", :temp3 => "no", :temp4 => "no", :temp5 => "no"}
+        elsif(@paramIn == "userinsurance")
+            render :template => "kioskpages/mainapp", :locals => {:temp1 => "yes", :temp2 => "no", :temp3 => "no", :temp4 => "no", :temp5 => "no"}
+        elsif(@paramIn == "userhealth") 
+            render :template => "kioskpages/mainapp", :locals => {:temp1 => "yes", :temp2 => "yes", :temp3 => "no", :temp4 => "no", :temp5 => "no"}
+        elsif(@paramIn == "userconsent")
+            render :template => "kioskpages/mainapp", :locals => {:temp1 => "yes", :temp2 => "yes", :temp3 => "yes", :temp4 => "no", :temp5 => "no"}
         elsif(@paramIn == "userreview")
-            render :template => "kioskpages/mainapp", :locals => {:temp1 => "no", :temp2 => "no", :temp3 => "no", :temp4 => "no", :temp5 => "yes"}
+            render :template => "kioskpages/mainapp", :locals => {:temp1 => "yes", :temp2 => "yes", :temp3 => "yes", :temp4 => "yes", :temp5 => "no"}
         else
             render :template => "kioskpages/mainapp", :locals => {:temp1 => "no", :temp2 => "no", :temp3 => "no", :temp4 => "no", :temp5 => "no"}
         end
