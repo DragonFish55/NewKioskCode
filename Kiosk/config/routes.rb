76946a5aed115f0c7e1086236afdf6e077029d96
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2021-12-04 11:34:30 -0500

    new changes

--- Kiosk/config/routes.rb
+++ Kiosk/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   resources :health_histories
+
+  get '/userdetails', to: "kioskpages#userdetails"
+  get '/userconsent', to: "kioskpages#userconsent"
+  get '/userinsurance', to: "kioskpages#userinsurance"
+  get '/userhealth', to: "kioskpages#userdetails"
+  get '/userreview', to: "kioskpages#userreview"
+
+  get '/signup', to: "kioskpages#signup"
+  get "/signin", to: "kioskpages#signin"
+
+  get '/login', to: "kioskqueries#login"
+  get '/login', to: "kioskqueries#logout"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
