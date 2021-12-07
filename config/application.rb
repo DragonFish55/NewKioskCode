54fa19ab0914ba17623e8ea70b72f50fc366770f
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2021-12-06 19:35:56 -0500

    added cors

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,6 @@ module Kiosk
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-      end
-    end
 
     # Configuration for the application, engines, and railties goes here.
     #
